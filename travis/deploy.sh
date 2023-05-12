fca163843110c68578d959f2950b31ecd846670b
searce-yug <yug.soni@searce.com>
2023-05-12 14:43:03 +0530

    resolving permission error

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -12,6 +12,8 @@ echo ${SA_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud auth configure-docker europe-west2-docker.pkg.dev -y
+docker-credential-gcr configure-docker --registries=europe-west2-docker.pkg.dev
+gcloud artifacts locations list
 docker build -t springapp-test:latest .
 docker tag springapp-test:latest europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
 docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
