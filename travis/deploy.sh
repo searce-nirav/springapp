e579bd4cf4f7e64d660c8f671d1bbb9a9657591f
searce-yug <yug.soni@searce.com>
2023-05-12 13:00:55 +0530

    fdghjk

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,10 +6,11 @@ curl https://sdk.cloud.google.com | bash -s -- --disable-prompts > /dev/null
 export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
 #gcloud --quiet components install kubectl
 
+gcloud config set project ${PROJECT_ID}
 echo ${SA_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-gcloud config set project ${PROJECT_ID}
+
 docker build -t springapp-test:latest .
 docker tag springapp-test:latest europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
 docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
