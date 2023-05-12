fe2152f16e9d5e5e41ea64f7ec52f2f1943dcc49
searce-yug <yug.soni@searce.com>
2023-05-12 13:05:33 +0530

    fdghjk

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -10,7 +10,7 @@ gcloud config set project ${PROJECT_ID}
 echo ${SA_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-
+gcloud auth configure-docker europe-west2-docker.pkg.dev -y
 docker build -t springapp-test:latest .
 docker tag springapp-test:latest europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
 docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
