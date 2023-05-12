92dc5d6c00df99be6a2b40a94e31503d89a4a4c0
searce-yug <yug.soni@searce.com>
2023-05-12 14:34:26 +0530

    resolving permission error

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 gcloud version || true
-#if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
 
-curl https://sdk.cloud.google.com | bash -s -- --disable-prompts > /dev/null
-export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
+#curl https://sdk.cloud.google.com | bash -s -- --disable-prompts > /dev/null
+#export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
 #gcloud --quiet components install kubectl
 
 gcloud config set project ${PROJECT_ID}
