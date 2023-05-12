7b8178b1faf679444bd651e881c185bbff9eab52
searce-yug <yug.soni@searce.com>
2023-05-12 17:55:48 +0530

    resolving permission error

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,16 +6,20 @@ curl https://sdk.cloud.google.com | bash -s -- --disable-prompts > /dev/null
 export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
 #gcloud --quiet components install kubectl
 
-gcloud auth login
-
 gcloud components update
 echo ${SA_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account ${SA_NAME} --key-file ${HOME}/gcloud-service-key.json
 gcloud config set project ${PROJECT_ID}
 
-gcloud auth configure-docker europe-west2-docker.pkg.dev -y
-docker-credential-gcr configure-docker --registries=europe-west2-docker.pkg.dev
-gcloud artifacts locations list
+#gcloud auth configure-docker europe-west2-docker.pkg.dev -y
+#docker-credential-gcr configure-docker --registries=europe-west2-docker.pkg.dev
+#gcloud artifacts locations list
 docker build -t springapp-test:latest .
-docker tag springapp-test:latest europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
-docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
+docker tag springapp-test:latest gcr.io/${PROJECT_ID}/springapp-test:latest
+docker push gcr.io/${PROJECT_ID}/springapp-test:latest
+
+#docker tag springapp-test:latest europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
+#docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:
+
+
+#Try using Container Registry
