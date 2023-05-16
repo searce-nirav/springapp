41f9ae601a41d0243017da76fbd3265e44352138
searce-yug <yug.soni@searce.com>
2023-05-16 12:06:10 +0530

    pushing to container reg

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -16,7 +16,13 @@ gcloud config set project ${PROJECT_ID}
 #echo Y | sudo gcloud auth configure-docker
 gcloud auth configure-docker -y
 
-gcloud components install docker-credential-gcr
+VERSION=2.1.5
+OS=linux  # or "darwin" for OSX, "windows" for Windows.
+ARCH=amd64  # or "386" for 32-bit OSs, "arm64" for ARM 64.
+
+curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
+| tar xz docker-credential-gcr \
+&& chmod +x docker-credential-gcr && sudo mv docker-credential-gcr /usr/bin/
 
 docker-credential-gcr configure-docker
 docker build -t springapp-test:latest .
