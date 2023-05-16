0187cd64ba9d7880caa73ef93fe417dbf8963177
searce-yug <yug.soni@searce.com>
2023-05-16 17:11:38 +0530

    pushing to container reg

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,9 +2,18 @@
 #gcloud version || true
 #if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
 
-curl https://sdk.cloud.google.com | bash -s -- --disable-prompts > /dev/null
-export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
+#curl https://sdk.cloud.google.com | bash -s -- --disable-prompts > /dev/null
+#export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
 #gcloud --quiet components install kubectl
+
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-430.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-430.0.0-linux-x86.tar.gz
+./google-cloud-sdk/install.sh
+./google-cloud-sdk/bin/gcloud init
+
+
+
+
 
 #gcloud components update
 echo ${SA_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
