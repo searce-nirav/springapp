29725f22be9527bb2ec32fc3b0a21f837863b49c
searce-yug <yug.soni@searce.com>
2023-05-16 11:19:11 +0530

    pushing to container reg

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -11,12 +11,12 @@ echo ${SA_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account ${SA_NAME} --key-file ${HOME}/gcloud-service-key.json
 gcloud config set project ${PROJECT_ID}
 
-echo Y | sudo gcloud auth configure-docker
-# docker-credential-gcr configure-docker
-#gcloud artifacts locations list
+#echo Y | sudo gcloud auth configure-docker
+gcloud auth configure-docker
+docker-credential-gcr configure-docker
 docker build -t springapp-test:latest .
-docker tag springapp-test:latest gcr.io/${PROJECT_ID}/spring-app/springapp-test:latest
-docker push gcr.io/${PROJECT_ID}/spring-app/springapp-test:latest
+docker tag springapp-test:latest gcr.io/${PROJECT_ID}/springapp-test:latest
+docker push gcr.io/${PROJECT_ID}/springapp-test:latest
 
 #docker tag springapp-test:latest europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
 #docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:
