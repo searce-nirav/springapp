e3623112e86a02c7d4e5aa70cc1d1d357993f65e
searce-yug <yug.soni@searce.com>
2023-05-16 11:41:42 +0530

    pushing to container reg

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,6 +6,8 @@ curl https://sdk.cloud.google.com | bash -s -- --disable-prompts > /dev/null
 export PATH=${HOME}/google-cloud-sdk/bin:${PATH}
 #gcloud --quiet components install kubectl
 
+sudo -s
+
 gcloud components update
 echo ${SA_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account ${SA_NAME} --key-file ${HOME}/gcloud-service-key.json
@@ -14,13 +16,7 @@ gcloud config set project ${PROJECT_ID}
 #echo Y | sudo gcloud auth configure-docker
 gcloud auth configure-docker -y
 
-VERSION=2.1.5
-OS=linux  # or "darwin" for OSX, "windows" for Windows.
-ARCH=amd64  # or "386" for 32-bit OSs, "arm64" for ARM 64.
-
-curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
-| tar xz docker-credential-gcr \
-&& chmod +x docker-credential-gcr && sudo mv docker-credential-gcr /usr/bin/
+gcloud components install docker-credential-gcr
 
 docker-credential-gcr configure-docker
 docker build -t springapp-test:latest .
