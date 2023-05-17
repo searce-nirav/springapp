9e79e32fb5fcf5ff4f2d2fe4434a8200231ed4a3
searce-yug <yug.soni@searce.com>
2023-05-17 16:20:29 +0530

    check the root user

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -45,13 +45,13 @@ echo Y | sudo gcloud auth configure-docker
 
 
 #2
-VERSION=2.1.5
-OS=linux  # or "darwin" for OSX, "windows" for Windows.
-ARCH=amd64  # or "386" for 32-bit OSs, "arm64" for ARM 64.
+# VERSION=2.1.5
+# OS=linux  # or "darwin" for OSX, "windows" for Windows.
+# ARCH=amd64  # or "386" for 32-bit OSs, "arm64" for ARM 64.
 
-curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
-| tar xz docker-credential-gcr \
-&& chmod +x docker-credential-gcr && sudo mv docker-credential-gcr /usr/bin/
+# curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
+# | tar xz docker-credential-gcr \
+# && chmod +x docker-credential-gcr && sudo mv docker-credential-gcr /usr/bin/
 
 
 #3
