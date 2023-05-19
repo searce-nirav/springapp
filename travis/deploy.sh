b3b81c0e2c620412edfec90e633381f3db012978
searce-yug <yug.soni@searce.com>
2023-05-19 11:41:48 +0530

    deploying to kubernetes cluster

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -15,16 +15,13 @@ gcloud --quiet components install kubectl
 echo ${SA_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account ${SA_NAME} --key-file ${HOME}/gcloud-service-key.json
 
-#gcloud config set account ${SA_NAME}
-gcloud config set project ${PROJECT_ID}
 
+gcloud config set project ${PROJECT_ID}
+gcloud container clusters get-credentials ${CLUSTER_NAME} --zone asia-south1-a --project ${PROJECT_ID}
 
 
 #1
 echo Y | gcloud auth configure-docker europe-west2-docker.pkg.dev
-
-#sudo chown -R travis:docker /home/travis/.docker
-#sudo chmod 777 "/home/travis/.docker"
 
 #2
 VERSION=2.1.5
@@ -44,6 +41,15 @@ docker tag springapp-test:latest europe-west2-docker.pkg.dev/${PROJECT_ID}/travi
 docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
 
 
+#deploy to kubernetes cluster
+kubectl apply -f deployment.yaml
+
+#get status of deployement
+kubectl get deployments
+
+kubectl get pods
+
+kubectl get services
 
 #FOR CONTAINER REGISTERY
 #sudo docker tag springapp-test:latest us.gcr.io/${PROJECT_ID}/app-engine-tmp/app/my-first-service/ttl-18h/springapp-test:latest
@@ -55,3 +61,6 @@ docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test
 # sudo groupadd docker
 # sudo useradd travis
 # sudo usermod -a -G docker travis
+
+#sudo chown -R travis:docker /home/travis/.docker
+#sudo chmod 777 "/home/travis/.docker"
