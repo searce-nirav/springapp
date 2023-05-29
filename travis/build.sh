654ce21812e30bb9fd31230a301e5a9ffc9dbca3
searce-yug <yug.soni@searce.com>
2023-05-29 10:02:23 +0530

    changed build script file name

--- travis/build.sh
+++ travis/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 gcloud --quiet components install kubectl
-
 
 echo ${SA_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account ${SA_NAME} --key-file ${HOME}/gcloud-service-key.json
@@ -17,3 +15,14 @@ echo Y | gcloud auth configure-docker europe-west2-docker.pkg.dev
 docker build -t springapp-test:latest .
 docker tag springapp-test:latest europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
 docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
+
+
+#to update the image in k8 deployment
+kubectl set image deployment/travis-spring-app spring-app=europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
+
+# #get status of deployement
+#kubectl get deployments
+
+kubectl get pods
+
+kubectl get services
--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,25 +1,3 @@
-#!/usr/bin/env bash
-
-#check what is available on the base image (vm specs)
-df -h
-
-gcloud --quiet components install kubectl
-
-
-echo ${SA_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account ${SA_NAME} --key-file ${HOME}/gcloud-service-key.json
-
-
-gcloud config set project ${PROJECT_ID}
-gcloud container clusters get-credentials ${CLUSTER_NAME} --zone asia-south1-a --project ${PROJECT_ID}
-
-
-echo Y | gcloud auth configure-docker europe-west2-docker.pkg.dev
-
-
-docker build -t springapp-test:latest .
-docker tag springapp-test:latest europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
-docker push europe-west2-docker.pkg.dev/${PROJECT_ID}/travis-test/springapp-test:latest
 
 
 #deploy to kubernetes cluster
@@ -34,6 +12,11 @@ kubectl set image deployment/travis-spring-app spring-app=europe-west2-docker.pk
 kubectl get pods
 
 kubectl get services
+
+
+
+
+
 
 
 #FOR CONTAINER REGISTERY
