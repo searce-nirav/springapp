0d7f4a175a6ebaf50b2d41a9c1ca2d924fae275c
searce-yug <yug.soni@searce.com>
2023-05-30 11:12:48 +0530

    demo

--- src/main/java/net/goinit/springboothelloworld/HelloWorldController.java
+++ src/main/java/net/goinit/springboothelloworld/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/")
 	public String sayHello() {
-		return "Hello from Cloud Run!! Extended Cloud Build";
+		return "Hello from Cloud Run!! Extended Cloud Build version-2";
 	}
 	
 }
